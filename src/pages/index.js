30e630022ada48e66eea83c42a2d0cf2e0a57970
Imed Adel <aadelimed@gmail.com>
2019-05-18 17:18:52 +0200

    Add index header

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,12 +23,26 @@ const BlogIndex = ({ data }, location) => {
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
       <Bio />
-      {posts.map(({ node }) => {
-        postCounter++
-        return (
-          <PostCard key={node.fields.slug} count={postCounter} node={node} postClass={`post`} />
-        )
-      })}
+      {data.site.siteMetadata.description && (
+        <header className="page-head">
+          <h2 className="page-head-title">
+            {data.site.siteMetadata.description}
+          </h2>
+        </header>
+      )}
+      <div className="post-feed">
+        {posts.map(({ node }) => {
+          postCounter++
+          return (
+            <PostCard
+              key={node.fields.slug}
+              count={postCounter}
+              node={node}
+              postClass={`post`}
+            />
+          )
+        })}
+      </div>
     </Layout>
   )
 }
@@ -38,6 +52,7 @@ const indexQuery = graphql`
     site {
       siteMetadata {
         title
+        description
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
