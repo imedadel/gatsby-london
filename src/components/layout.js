2cd615b6b9bb9b3eb99edb40fb4b14a9ea59789f
Imed Adel <aadelimed@gmail.com>
2019-05-18 17:46:48 +0200

    Add default layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,15 +21,80 @@ class Layout extends React.Component {
       )
     }
     return (
-      <>
-        <header>{header}</header>
-        <main>{children}</main>
+      <div className="site-wrapper">
+        <header className="site-head">
+          <div className="site-head-container">
+            <Link className="nav-burger" to={`/`}>
+              <div
+                className="hamburger hamburger--collapse"
+                aria-label="Menu"
+                role="button"
+                aria-controls="navigation"
+              >
+                <div className="hamburger-box">
+                  <div className="hamburger-inner" />
+                </div>
+              </div>
+            </Link>
+            <nav id="swup" class="site-head-left">
+              <ul className="nav" role="menu">
+                <li className="nav-home nav-current" role="menuitem">
+                  <a href="https://london.ghost.io/">Home</a>
+                </li>
+                <li className="nav-about" role="menuitem">
+                  <a href="https://london.ghost.io/about/">About</a>
+                </li>
+                <li className="nav-elements" role="menuitem">
+                  <a href="https://london.ghost.io/elements/">Elements</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="site-head-center">
+              <Link className="site-head-logo" to={`/`}>
+                {title}
+              </Link>
+            </div>
+            <div className="site-head-right">
+              <div className="social-links">
+                <a
+                  href="https://www.facebook.com/ghost"
+                  title="Facebook"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Facebook
+                </a>
+                <a
+                  href="https://twitter.com/tryghost"
+                  title="Twitter"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Twitter
+                </a>
+                <a
+                  href="https://feedly.com/i/subscription/feed/https://london.ghost.io/rss/"
+                  title="RSS"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  RSS
+                </a>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main id="site-main" className="site-main">
+          <div id="swup" className="transition-fade">
+            {children}
+          </div>
+        </main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </>
+      </div>
     )
   }
 }
