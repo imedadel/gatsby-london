af2a285889f18902cfc4fdec26133dc5352f3f0c
Imed Adel <aadelimed@gmail.com>
2019-05-19 14:22:16 +0200

    Add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,13 +17,44 @@ const AboutPage = ({ data }, location) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO
-        title="All posts"
+        title="ABout"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
-      {/* <Bio /> */}
-      <article className="post-content no-image">
+
+      <article className="post-content page-template no-image">
         <div className="post-content-body">
-          <p>Je m'appelle Imed Adel</p>
+          <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
+            Clean, minimal, and deeply customisable. London is a theme made for
+            people who appreciate simple lines.
+          </h2>
+          <figure className="kg-card kg-image-card kg-width-full">
+            <Img
+              fluid={data.benchAccounting.childImageSharp.fluid}
+              className="kg-image"
+            />
+            <figcaption>Large imagery is at the heart of this theme</figcaption>
+          </figure>
+          <h3 id="dynamic-styles">Dynamic styles</h3>
+          <p>
+            London comes with photo-centric main layout best suited to
+            photography, graphics portfolios and other image-heavy uses.
+          </p>
+          <p>
+            Both post and page templates are light and minimal, with all the
+            focus on the content while the design of the theme gets out of the
+            way. Beneath the hood, London enjoys the full power of the{" "}
+            <a href="https://docs.ghost.org/api/handlebars-themes/">
+              Ghost Handlebars Theme API
+            </a>{" "}
+            to provide limitless customisation options and dynamic styles.
+          </p>
+          <p>
+            Don't forget to check out the{" "}
+            <a href="https://docs.ghost.org/integrations/">
+              Ghost Integrations Directory
+            </a>{" "}
+            for more ways to integrate Ghost with your favourite services.
+          </p>
         </div>
       </article>
     </Layout>
@@ -35,28 +66,12 @@ const indexQuery = graphql`
     site {
       siteMetadata {
         title
-        description
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 1360) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
+    benchAccounting: file(relativePath: { eq: "bench-accounting-49909-unsplash.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
