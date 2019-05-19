eb665071f68c6f898896607bfb605f6405f7a26f
Imed Adel <aadelimed@gmail.com>
2019-05-19 14:22:34 +0200

    Add site configuration file

---
+++ siteConfig.js
@@ -0,0 +1,7 @@
+export default {
+  name: `London`,
+  shortName: `London`,
+  description: `A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery`,
+  url: `https://gatsby-london.netlify.com`,
+  author: `Imed Adel`,
+}
