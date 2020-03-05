3ed0a5bbca6cf1a39e64c529eefdd711eb454b9c
Promisemasimba <57047533+Promisemasimba@users.noreply.github.com>
2020-03-05 10:57:26 +0200

    Site Name and Author
    Site Name and AuthorS details

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
+  name: 'Citrix Digital',
+  shortName: 'Cd.',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
   url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
   prefix: '/',
-  author: 'Imed Adel',
-  twitter: 'Imed_Adel'
+  author: 'Promise Masimba',
+  twitter: 'Promise Masimba'
 }
