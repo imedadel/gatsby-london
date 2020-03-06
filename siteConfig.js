458c1070ead3bef49a75ab3f6c5afff76aa4b033
Promisemasimba <57047533+Promisemasimba@users.noreply.github.com>
2020-03-06 02:35:10 +0200

    Site name and description

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'Citrix Digital',
   shortName: 'Cd.',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  description: 'Helping People Through Technology.',
+  url: 'https://citrixdigital.com', // Domain of your site without prefix!
   prefix: '/',
   author: 'Promise Masimba',
   twitter: 'Promise Masimba'
